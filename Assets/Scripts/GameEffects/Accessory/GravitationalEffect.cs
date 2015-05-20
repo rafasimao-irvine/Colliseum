b447df569d56e019b7251fc4ecb037cdae16f123
rafael simao <raficher@gmail.com>
2015-05-20 13:49:13 -0300

    Granada Gravitacional
    Criada o acessório ativo: Granada Gravitacional

---
+++ Assets/Scripts/GameEffects/Accessory/GravitationalEffect.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GravitationalEffect : GameEffect {
+
+	protected override void DoEffect (Interactive origin, Interactive target) {
+		if (target.MyTile != null)
+			DoEffect(origin, target.MyTile);
+	}
+
+	protected override void DoEffect (Interactive origin, Tile targetTile) {
+
+		List<Tile> neighbours = MapController.Instance.GetNeighbours(targetTile,_EffectRange);
+		//MapController.Instance.SortTilesByDistanceTo(neighbours, targetTile);
+
+		List<Tile> selectedTiles = new List<Tile>();
+
+		for (int i=0; i<neighbours.Count; i++) {
+			if (neighbours[i].OnTop!=null && neighbours[i].OnTop is Characther) {
+				Vector2 direction = MapController.Instance.GetDirection(neighbours[i],targetTile);
+				Tile next = MapController.Instance.GetNextTile(neighbours[i], direction);
+
+				if (next!=null && 
+				    (next.OnTop==null || (next.OnTop!=null && !next.OnTop.Blockable)) &&
+				    !selectedTiles.Contains(next) ) {
+					((Characther)neighbours[i].OnTop).AddMovementFeat(new SlideFeat(next));
+					if (neighbours[i].OnTop is Personage) 
+						((Personage)neighbours[i].OnTop).InterruptActions();
+
+					selectedTiles.Add(next);
+				}
+			}
+		}
+
+	}
+
+}
