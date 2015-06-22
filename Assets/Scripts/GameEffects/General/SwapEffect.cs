6d83eb40bf857c60a7df561a29d6e0307efc4012
rafael simao <raficher@gmail.com>
2015-06-22 11:48:50 -0300

    Created SwapEffect
    Criado o efeito de troca de posição com um outro personagem.

---
+++ Assets/Scripts/GameEffects/General/SwapEffect.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwapEffect : GameEffect {
+
+	protected override void DoEffect (Interactive origin, Interactive target) {
+		if (origin!=null && origin.GetBeAttackedTarget() is Characther && 
+		    target!=null && target.GetBeAttackedTarget() is Characther) {
+			origin = origin.GetBeAttackedTarget();
+			target = target.GetBeAttackedTarget();
+
+			MapController map = MapController.Instance;
+
+			Tile oTile = origin.MyTile;
+			Tile tTile = target.MyTile;
+
+			oTile.TryGetOut(origin);
+			tTile.TryGetOut(target);
+
+			map.PlaceItAt(origin,tTile);
+			map.PlaceItAt(target,oTile);
+		}
+	}
+
+}
