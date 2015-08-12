957d995770da671ddfc1f9fcc45fbee06085a505
rafael simao <raficher@gmail.com>
2015-08-12 14:12:28 -0300

    Bug fix: Don't get invisible when dead anymore

--- Assets/Scripts/GameEffects/General/InvisibleEffect.cs
+++ Assets/Scripts/GameEffects/General/InvisibleEffect.cs
@@ -7,7 +7,7 @@ public class InvisibleEffect : GameEffect {
 	private int _NTurns;
 	
 	protected override void DoEffect (Interactive origin, Interactive target) {
-		if (target is Characther)
+		if (target is Characther && !((Characther)target).IsDead())
 			((Characther)target).BeInvisible(_NTurns);
 	}
 
